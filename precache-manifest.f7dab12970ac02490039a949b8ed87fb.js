93740b7697c01bb55d401c0741a92eef5d9688f0
Vasiliy Yurkovets <yurkovets@Vasiliys-MacBook-Pro.local>
2019-07-16 20:55:50 +0300

    Updates

---
+++ precache-manifest.f7dab12970ac02490039a949b8ed87fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "532b6dccf8070b1353bcb11b3dbf57f0",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "29772ad5c91bd9336958",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.9c6cf29e.chunk.css"
+  },
+  {
+    "revision": "8ab387c770754f274a20",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.20dca77c.chunk.js"
+  },
+  {
+    "revision": "29772ad5c91bd9336958",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.1e61d30c.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
